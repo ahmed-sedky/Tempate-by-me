3927ea40a043e8398c42bfb8aa0997619cb0cc8b
ahmed-sedky <61352734+ahmed-sedky@users.noreply.github.com>
2020-07-11 23:23:46 +0200

    second commit

---
+++ JS/index.js
@@ -0,0 +1,28 @@
+var myDiv1 =document.getElementById('skills'),
+    myDiv2 =document.getElementById('laptop'),
+    myDiv3 =document.getElementById('About'),
+    myDiv4 =document.getElementById('button');
+    myDiv5 =document.getElementById('resume');
+    myDiv6 =document.getElementById('Images');
+    window.onscroll = function () {
+    "use strict";
+        if (document.documentElement.scrollTop >= myDiv1.offsetTop -700 ) { //offsetTop returns the top position in pixels
+            myDiv1.classList.add('flash');
+        }
+        if (document.documentElement.scrollTop >= myDiv2.offsetTop -100 ) { 
+            myDiv2.classList.add('fadeInLeft');
+        }
+        if (document.documentElement.scrollTop >= myDiv3.offsetTop -100 ) { 
+            myDiv3.classList.add('fadeInRight');
+        } 
+        if (document.documentElement.scrollTop >= myDiv4.offsetTop -550 ) { 
+            myDiv4.classList.add('rubberBand');
+        }
+        if (document.documentElement.scrollTop >= myDiv5.offsetTop -550 ) { 
+            myDiv5.classList.add('bounce');
+        }
+        if (document.documentElement.scrollTop >= myDiv6.offsetTop  ) { 
+            myDiv6.classList.add('heartBeat');
+        }
+};
+
